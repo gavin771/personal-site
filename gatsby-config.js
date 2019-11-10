43e037ae9de6aee089498e58b55eb1659fdfed10
Gavin Samuels <gavin.samuels10@gmail.com>
2019-11-10 01:04:32 -0500

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,31 @@ module.exports = {
                 pageContextProperty: `menus`,
                 menus: require('./src/data/menus.json'),
             }
-        }
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+              trackingId: "UA-151955170-1",
+              // Defines where to place the tracking script - `true` in the head and `false` in the body
+              head: true,
+              // Setting this parameter is optional
+              anonymize: true,
+              // Setting this parameter is also optional
+              respectDNT: true,
+              // Avoids sending pageview hits from custom paths
+              // exclude: ["/preview/**", "/do-not-track/me/too/"],
+              // Delays sending pageview hits on route update (in milliseconds)
+              pageTransitionDelay: 0,
+              // Enables Google Optimize using your container Id
+              optimizeId: "",
+              // Enables Google Optimize Experiment ID
+              experimentId: "",
+              // Set Variation ID. 0 for original 1,2,3....
+              variationId: "",
+              // Any additional optional fields
+            //   sampleRate: 5,
+            //   siteSpeedSampleRate: 10,
+            //   cookieDomain: "example.com",
+            },
     ]
 };
