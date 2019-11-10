ca263c007c86dba29ef7ea525fe18a3c6d76faae
Gavin Samuels <gavin.samuels10@gmail.com>
2019-11-10 16:31:19 -0500

    export projects block from component index

--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,30 @@
-import ContentBlock from './ContentBlock';
-import Footer from './Footer';
-import Header from './Header';
-import HeroBlock from './HeroBlock';
-import PostsBlock from './PostsBlock';
-import Social from './Social';
-import Layout from './Layout';
+import ContentBlock from "./ContentBlock";
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroBlock from "./HeroBlock";
+import PostsBlock from "./PostsBlock";
+import Social from "./Social";
+import Layout from "./Layout";
+import ProjectsBlock from "./ProjectsBlock";
 
 export {
-    ContentBlock,
-    Footer,
-    Header,
-    HeroBlock,
-    PostsBlock,
-    Social,
-    Layout
+  ContentBlock,
+  Footer,
+  Header,
+  HeroBlock,
+  PostsBlock,
+  Social,
+  Layout,
+  ProjectsBlock
 };
 
 export default {
-    ContentBlock,
-    Footer,
-    Header,
-    HeroBlock,
-    PostsBlock,
-    Social,
-    Layout
+  ContentBlock,
+  Footer,
+  Header,
+  HeroBlock,
+  PostsBlock,
+  Social,
+  Layout,
+  ProjectsBlock
 };
