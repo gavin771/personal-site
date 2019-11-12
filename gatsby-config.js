32eb7dcbaffc4375ec14eea75a6ea62ebe80d207
Gavin Samuels <gavin.samuels10@gmail.com>
2019-11-11 19:00:06 -0500

    removed gatsby menus plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,68 +1,66 @@
 module.exports = {
-    pathPrefix: '/',
-    siteMetadata: require('./site-metadata.json'),
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-source-data`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-stackbit-static-sass`,
-            options: {
-                inputFile: `${__dirname}/src/sass/main.scss`,
-                outputFile: `${__dirname}/public/assets/css/main.css`
-            },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [`gatsby-remark-component`]
-            }
-        },
-        {
-            resolve: `gatsby-remark-page-creator`,
-            options: {
-                
-            }
-        },
-        {
-            resolve: `@stackbit/gatsby-plugin-menus`,
-            options: {
-                sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`,
-                menus: require('./src/data/menus.json'),
-            }
-        },
-        {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-              trackingId: "UA-151955170-1",
-              // Defines where to place the tracking script - `true` in the head and `false` in the body
-              head: true,
-              // Setting this parameter is optional
-              anonymize: true,
-              // Setting this parameter is also optional
-              respectDNT: true,
-              // Avoids sending pageview hits from custom paths
-              // exclude: ["/preview/**", "/do-not-track/me/too/"],
-              // Delays sending pageview hits on route update (in milliseconds)
-              pageTransitionDelay: 0,
-              // Enables Google Optimize using your container Id
-            //   optimizeId: "",
-              // Enables Google Optimize Experiment ID
-            //   experimentId: "",
-              // Set Variation ID. 0 for original 1,2,3....
-            //   variationId: "",
-              // Any additional optional fields
-            //   sampleRate: 5,
-            //   siteSpeedSampleRate: 10,
-            //   cookieDomain: "example.com",
-            }
-        }
-    ]
+  pathPrefix: "/",
+  siteMetadata: require("./site-metadata.json"),
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-source-data`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-stackbit-static-sass`,
+      options: {
+        inputFile: `${__dirname}/src/sass/main.scss`,
+        outputFile: `${__dirname}/public/assets/css/main.css`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-component`]
+      }
+    },
+    {
+      resolve: `gatsby-remark-page-creator`,
+      options: {}
+    },
+    // {
+    //     resolve: `@stackbit/gatsby-plugin-menus`,
+    //     options: {
+    //         sourceUrlPath: `fields.url`,
+    //         pageContextProperty: `menus`,
+    //         menus: require('./src/data/menus.json'),
+    //     }
+    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-151955170-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0
+        // Enables Google Optimize using your container Id
+        //   optimizeId: "",
+        // Enables Google Optimize Experiment ID
+        //   experimentId: "",
+        // Set Variation ID. 0 for original 1,2,3....
+        //   variationId: "",
+        // Any additional optional fields
+        //   sampleRate: 5,
+        //   siteSpeedSampleRate: 10,
+        //   cookieDomain: "example.com",
+      }
+    }
+  ]
 };
